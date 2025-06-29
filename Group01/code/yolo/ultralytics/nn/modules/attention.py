42b0a08f980f72311a65a8b95457bf3695f33065
Lemonbluemo <1216808410@qq.com>
2025-06-29 19:33:41 +0800

    code

---
+++ Group01/code/yolo/ultralytics/nn/modules/attention.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class ChannelAttention(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(channels, channels // reduction, 1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channels // reduction, channels, 1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.fc(y)
+        return x * y
